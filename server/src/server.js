0094ef888c0a4624e1cbf356f68a6bb1b6f0775c
unknown <blissimeonx@gmail.com>
2026-08-13 07:52:48 +0100

    correction in the server.js

--- server/src/server.js
+++ server/src/server.js
@@ -26,7 +26,7 @@ app.use(errorHandlerMiddleware);
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
 
-  app.get("/{*any}", (req, res, next) => {
+  app.get("*", (req, res, next) => {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }
